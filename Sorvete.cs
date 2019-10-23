60c1c04d39535ca2e3456a7b5e43cc034dd79efb
GhabrielFiorotti <ghabriel@ucl.br>
2019-10-23 17:47:54 -0300

    Colocando os códigos para o Sorvete.cs

--- Sorvete.cs
+++ Sorvete.cs
@@ -1,7 +1,39 @@
 ﻿using System;
 
-class Sorvete : Alimento{
-  private float preco=3.0f;
-  private float custo=1.5f;
- 
+class Sorvete : Alimento {
+  private float preco;
+  private string tipo;
+  private float custo;
+
+  private void TipoSorvete(string tipo){
+    if (tipo == "Casquinha") {
+      this.tipo = tipo;
+      this.preco= 3;
+      this.custo = 1.50f;
+      Grava.GravaCusto(custo);
+      Grava.GravaLucroBruto(preco);
+    }else if (tipo == "Copo") {
+      this.tipo = tipo;
+      this.preco= 2f;
+      this.custo = 1f; 
+      Grava.GravaCusto(custo);
+      Grava.GravaLucroBruto(preco);
+    }else if(tipo == "Milk Shake"){
+      this.tipo = tipo;
+      this.preco= 8.5f;
+      this.custo = 4;
+      Grava.GravaCusto(custo);
+      Grava.GravaLucroBruto(preco);
+    }
+  }
+
+  public void setNome(string tipo){
+    nome = tipo;
+  }
+
+  public void Retorno(){
+    Console.WriteLine("======================= {0} ========================",nome);
+    Console.WriteLine("Bebida\n");
+    Console.WriteLine("======================= Preco: {0} =========================",preco);
+  }
 }
